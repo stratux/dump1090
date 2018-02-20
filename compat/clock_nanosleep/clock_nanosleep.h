0657f57964ff50bce533c4a404d46f4a9d256a43
Claudio Basile <claudio.basile@gmail.com>
2018-02-19 20:34:50 -0800

    Fix build break on Mac OS

--- compat/clock_nanosleep/clock_nanosleep.h
+++ compat/clock_nanosleep/clock_nanosleep.h
@@ -1,21 +1,7 @@
 #ifndef CLOCK_NANOSLEEP_H
 #define CLOCK_NANOSLEEP_H
 
-#ifdef _CLOCKID_T_DEFINED_
-#define CLOCKID_T
-#endif
-
-#ifndef CLOCKID_T
-#define CLOCKID_T
-typedef enum
-{
-    CLOCK_REALTIME,
-    CLOCK_MONOTONIC,
-    CLOCK_PROCESS_CPUTIME_ID,
-    CLOCK_THREAD_CPUTIME_ID
-} clockid_t;
-#endif // ifndef CLOCKID_T
-
+#include <time.h>
 
 #ifndef TIMER_ABSTIME
 #define TIMER_ABSTIME 1
